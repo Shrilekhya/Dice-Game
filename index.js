bf61dd21d31b9f2e544213a7dfe54cc6abd4f5ef
Shrilekhya <109731398+Shrilekhya@users.noreply.github.com>
2023-01-20 08:03:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var randomNum1 = Math.random();
 randomNum1 = Math.floor(randomNum1*6 +1);
 var loc1 = "dice" + randomNum1 + ".png" ;
-var loc11 = "images/" + loc1;
-document.querySelectorAll("img")[0].setAttribute("src" , loc11);
+
+document.querySelectorAll("img")[0].setAttribute("src" , loc1);
 
 var randomNum2 = Math.random();
 randomNum2 = Math.floor(randomNum2*6 +1);
 var loc2 = "dice" + randomNum2 + ".png" ;
-var loc22 = "images/" + loc2;
-document.querySelectorAll("img")[1].setAttribute("src" , loc22);
+
+document.querySelectorAll("img")[1].setAttribute("src" , loc2);
 
 if(randomNum1>randomNum2){
     document.getElementsByTagName("h1")[0].textContent = "🚩Player 1 Wins!";
